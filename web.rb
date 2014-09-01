506b8caaf9f13c1ae852ca199ef064d25471febf
Vivek Bhagwat <vbhagwat@twitter.com>
2014-09-01 19:53:46 -0400

    add protection logic

--- web.rb
+++ web.rb
@@ -25,3 +25,15 @@ def send_to_template(template, vars)
   erb template, :locals => vars
 end
 
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', 'admin']
+  end
+end
