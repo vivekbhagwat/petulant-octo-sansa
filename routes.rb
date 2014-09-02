781a1ffc2b7e2dae36541ec1e2ff244a45870cf6
Vivek Bhagwat <vbhagwat@twitter.com>
2014-09-01 21:58:42 -0400

    update image again

--- routes.rb
+++ routes.rb
@@ -10,7 +10,7 @@ bio = '''
 Sofia Montgomery was born and raised in San Francisco, CA and moved to New York in 2009 to attend Fordham University. Throughout her childhood Sofia was involved in theatre and discovered her love of stage management at the age of 13.  Since then Sofia has pursued stage management in and outside of school, and has expanded that interest to company management.  Sofia has stage managed a variety of performances at Fordham University and in New York at various festivals and with Less Than Rent Theatre Company where she is a company member and company manager.  Sofia has also worked for companies such as Production Glue, CW & Co. and The Lawrence Company Events on corporate events and galas.  Last May Sofia graduated from Fordham University and soon became an AEA stage manager.  Sofia is currently the assistant stage manager of Summer Shorts  at 59E59.
 '''
 title = 'Bio'
-image = 'http://i.imgur.com/Tu0bDyt.png'
+image = 'http://i.imgur.com/eDilyQy.jpg'
 
   send_to_template :bio, {:title => title, :bio => bio, :image => image}
 end
