4fc314361a29b1f28a806931e52e93c37ebb9178
Vivek Bhagwat <vbhagwat@twitter.com>
2014-09-01 22:24:27 -0400

    update resume change text and add caption

--- routes.rb
+++ routes.rb
@@ -7,16 +7,19 @@ end
 get '/bio' do
 
 bio = '''
-Sofia Montgomery was born and raised in San Francisco, CA and moved to New York in 2009 to attend Fordham University. Throughout her childhood Sofia was involved in theatre and discovered her love of stage management at the age of 13.  Since then Sofia has pursued stage management in and outside of school, and has expanded that interest to company management.  Sofia has stage managed a variety of performances at Fordham University and in New York at various festivals and with Less Than Rent Theatre Company where she is a company member and company manager.  Sofia has also worked for companies such as Production Glue, CW & Co. and The Lawrence Company Events on corporate events and galas.  Last May Sofia graduated from Fordham University and soon became an AEA stage manager.  Sofia is currently the assistant stage manager of Summer Shorts  at 59E59.
+Sofia Montgomery was born and raised in San Francisco, CA and moved to New York in 2009 to attend Fordham University. Throughout her childhood Sofia was involved in theatre and discovered her love of stage management at the age of 13.  Since then Sofia has pursued stage management in and outside of school, and has expanded that interest to company management.  Sofia has stage managed a variety of performances at Fordham University and in New York at various festivals and with Less Than Rent Theatre Company where she is a company member and company manager.  Sofia has also worked for companies such as Production Glue, CW & Co. and The Lawrence Company Events on corporate events and galas.  Last May Sofia graduated from Fordham University and soon became an AEA stage manager.  Sofia is currently the production stage manager of a developmental workshop of <i>Coal The Musical</i>.
 '''
 title = 'Bio'
 image = 'http://i.imgur.com/eDilyQy.jpg'
+caption = "Photo Credit Mai Demeterio"
 
-  send_to_template :bio, {:title => title, :bio => bio, :image => image}
+  send_to_template :bio, {:title => title, :bio => bio, :image => image, :caption => caption}
 end
 
 get '/projects' do
-  projects = "Sofia is currently the assistant stage manager of Summer Shorts at 59E59.  Sofia is available for work again in September of 2014 to stage manage theatre or events."
+  projects = '''
+Sofia is currently the production stage manager of a developmental workshop of <i>Coal The Musical</i>.  Sofia is available for work again in September of 2014 to stage manage theatre or events.
+'''
   image = "http://i.imgur.com/CRksCgk.jpg"
   caption = "Rehearsal photo from Stockholm
 Photo Credit Russ Rowland"
@@ -24,7 +27,7 @@ Photo Credit Russ Rowland"
 end
 
 get '/resume' do
-  image = "http://i.imgur.com/4NC1Mzi.jpg"
+  image = "http://i.imgur.com/mPsi4wJ.jpg"
   send_to_template :resume, {:image => image}
 end
 
