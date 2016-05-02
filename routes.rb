2582c5c39bcf1de7b29011500bb13e644abcb976
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2016-05-02 12:55:12 -0400

    May 2016

--- routes.rb
+++ routes.rb
@@ -7,7 +7,7 @@ end
 get '/bio' do
 
 bio = '''
-Sofia Montgomery was born and raised in San Francisco, CA and moved to New York in 2009 to attend Fordham University, majoring in stage management. Throughout her childhood Sofia was involved in theatre and discovered her love of stage management at the age of 13. Sofia continued to pursue stage management at Fordham and has enjoyed the start of an exciting career as an AEA stage manager. Sofia has stage managed a variety of performances at venues such as New World Stages and 59E59. Sofia has stage managed many new works as well as musicals and has expanded her skills to also stage manage events. In the event world Sofia has worked for companies such as Production Glue and CW & Co. on corporate events and galas. Sofia graduated from Fordham University in 2013 and soon became an AEA stage manager. Sofia\'s favorite recent projects include a workshop of <i>Prosthesis</i> directed by Moritz von Stuelpnagel and written by Robert Askins, several exciting new productions with One Year Lease Theater Company and volunteering as a stage manager for Broadway Cares.
+Sofia Montgomery was born and raised in San Francisco, CA and moved to New York in 2009 to attend Fordham University, majoring in stage management. Throughout her childhood Sofia was involved in theatre and discovered her love of stage management at the age of 13. Sofia continued to pursue stage management at Fordham and has enjoyed the start of an exciting career as an AEA stage manager. Sofia has stage managed a variety of performances at venues such as New World Stages and 59E59. Sofia has stage managed many new works as well as musicals and has expanded her skills to also stage manage events. In the event world Sofia has worked for companies such as Production Glue and CW & Co. on corporate events and galas. Sofia graduated from Fordham University in 2013 and soon became an AEA stage manager. Sofia\'s favorite recent projects include a workshop of <i>Prosthesis</i> directed by Moritz von Stuelpnagel and written by Robert Askins, the Pipeline Festival at the Women\'s Project, several exciting new productions with One Year Lease Theater Company and volunteering as a stage manager for Broadway Cares.
 '''
 title = 'Bio'
 image = 'http://i.imgur.com/eDilyQy.jpg'
@@ -18,7 +18,7 @@ end
 
 get '/projects' do
   projects = '''
-Sofia is currently the PSM for <i>Kings</i> directed by Adrienne Campbell-Holt and written by Sarah Burgess at the Women\'s Project and is available for other opportunities.
+Sofia is currently the PSM for three shows in the Brits Off Broadway Festival at 59E59 and is available for other opportunities.
 '''
   image = "http://i.imgur.com/CRksCgk.jpg"
   caption = "Rehearsal photo from Stockholm
@@ -27,7 +27,7 @@ Photo Credit Russ Rowland"
 end
 
 get '/resume' do
-  image = "http://i.imgur.com/rUPAw6O.png"
+  image = "http://i.imgur.com/N3qWjQS.png"
   send_to_template :resume, {:image => image}
 end
 
